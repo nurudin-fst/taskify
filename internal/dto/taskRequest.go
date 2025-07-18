18d3dff3d9a96152e1e0feb483ce1df1d630a340
nurudin-fst <nurudin.stevor@gmail.com>
2025-07-18 13:43:15 +0700

    [fix] add validation on task insert and update

--- internal/dto/taskRequest.go
+++ internal/dto/taskRequest.go
@@ -1,14 +1,14 @@
 package dto
 
 type TaskInsertIn struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Deadline    string `json:"deadline"`
+	Title       string `json:"title" validate:"required"`
+	Description string `json:"description"  validate:"required"`
+	Deadline    string `json:"deadline" validate:"required"`
 }
 
 type TaskUpdateIn struct {
-	Title       string `json:"title"`
-	Status      string `json:"status" validate:"oneof=todo in_progress done" enums:"todo,in_progress,done"`
-	Description string `json:"description"`
-	Deadline    string `json:"deadline"`
+	Title       string `json:"title"  validate:"required"`
+	Status      string `json:"status" validate:"required,oneof=todo in_progress done" enums:"todo,in_progress,done"`
+	Description string `json:"description" validate:"required"`
+	Deadline    string `json:"deadline" validate:"required"`
 }
